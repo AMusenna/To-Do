efc8e611e0129b9073c4c55d59a86054d1975beb
Amusenna <ahmet@visiosoft.com.tr>
2022-07-06 15:54:28 +0200

    Make an List and add somthing

--- app.js
+++ app.js
@@ -4,17 +4,31 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000
 
+var items = [];
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
   var today = new Date();
-  var dayName = today.toLocaleString('ge-us', {weekday: 'long'});;
 
 
-  res.render("list", {dayNameEn: dayName});
+  var dayName = today.toLocaleDateString('de-DE', {month: 'long', day: 'numeric', weekday: 'long'});
+
+  res.render("list", {dayNameEn: dayName, newListItem: items});
 
 });
+
+app.post("/", function(req, res){
+
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+})
 
 
 app.listen(port, () => {
