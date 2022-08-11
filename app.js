5b15b06d95ca8023fcfcff3f6b766103e38867d3
Amusenna <ahmet@visiosoft.com.tr>
2022-08-11 09:46:07 +0200

    Gitignore , procfile added

--- app.js
+++ app.js
@@ -129,7 +129,11 @@ app.post("/delete", function(req, res){
   }
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server has started Successfully ! ${port}`)
 });
